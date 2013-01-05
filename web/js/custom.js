fcbf2bfd548395a1df11b7f607e5a27b3c821619
Apoorv Naik <apoorvnaik@github.com>
2013-01-06 01:12:40 +0530

    Added landing page & DB edits added Group/Friend Table

---
+++ web/js/custom.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$('#shared-bill').click(function(){
+	alert("clicker");
+	$(this).addClass("active");
+	});
+$('#item-bill').click(function(){
+	$(this).addClass("active");
+	});
